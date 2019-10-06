db2cab46a0ba11dc2fecf15def9097b2eaa8b994
Gustavo Tavares de Souza <souza.gt2014@bol.com.br>
2019-10-06 13:08:33 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC App from c# Course";
+            ViewData["Creator"] = "Gustavo Tavares de Souza";
 
             return View();
         }
